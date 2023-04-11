65eb6fabdb594454022020040f2040dc9e795d0c
Kevin Klose <kevin.klose@teamdynamix.com>
2023-04-11 15:16:44 -0400

    Add project files.

---
+++ MonkeyFinder/Controls/CustomSearchHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace MonkeyFinder.Controls;
+
+public class CustomSearchHandler : SearchHandler
+{    
+  public CustomSearchHandler()
+  {
+    var viewModel = ServiceProvider.GetService<MonkeysViewModel>();
+    viewModel.SetCurrentSearchHandler(this);
+
+    Placeholder = "Search...";
+    ShowsResults = false;
+  }
+
+  public void SetQuery(string searchText)
+  {
+    Query = searchText;
+
+    //SearchBoxVisibility = SearchBoxVisibility.Hidden;
+    //SearchBoxVisibility = SearchBoxVisibility.Expanded;
+  }
+
+  public static IServiceProvider ServiceProvider
+    =>
+#if WINDOWS
+    MauiWinUIApplication.Current.Services;
+#elif ANDROID
+      MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+			MauiUIApplicationDelegate.Current.Services;
+#else
+    null;
+#endif
+}
